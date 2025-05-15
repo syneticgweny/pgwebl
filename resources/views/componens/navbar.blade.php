3f88fd5074f23183062c865cdade6ba028b2c034
syneticgweny <syneticgweny@gmail.com>
2025-05-15 15:12:08 +0700

    PGWL 9 : EDIT DATA

--- resources/views/componens/navbar.blade.php
+++ resources/views/componens/navbar.blade.php
@@ -17,25 +17,18 @@
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        <i class="fa-solid fa-database"></i> Data
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="{{ route('api.points')}}" target="_blank">Points</a></li>
+                        <li><a class="dropdown-item" href="{{ route('api.polylines')}}" target="_blank">Polylines</a></li>
+                        <li><a class="dropdown-item" href="{{ route('api.polygons')}}" target="_blank">Polygons</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
         </div>
     </div>
 </nav>
